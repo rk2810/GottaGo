b8c3d2b6e56b1c6d59dbc83848c700ec5b18f7c1
rk2810 <rohan.kumquat@gmail.com>
2018-06-18 09:17:57 +0530

    More about structs.
    Signed-off-by: rk2810 <rohan.kumquat@gmail.com>

--- Structs/structs.go
+++ Structs/structs.go
@@ -1,4 +1,5 @@
 package main
+
 import "fmt"
 
 // Foo is a struct here, We dont have inheritance in Go, and hence structs are different than objects
@@ -6,6 +7,7 @@ type Foo struct {
 	A int
 	b string
 }
+
 func main() {
 	f := Foo{}
 	fmt.Println(f)
@@ -14,6 +16,34 @@ func main() {
 	fmt.Println(g)
 
 	fmt.Println(g.b)
-	f.b = "Hey"
+	// Ways of adding values to structs
+
+	f.b = "Hey" // If you are not supplying value to one of the data type in struct, It automatically takes zero value
+			   // for it.
+	h := Foo{
+		A : 20,
+	}
+	
+	fmt.Println(h)
+
 	fmt.Println(f)
+
+	// Structs in Go are value type, example >
+
+	var f2 Foo
+	f2 = f
+	f2.A = 100
+
+	fmt.Println(f2.A)
+	fmt.Println(f.A)
+
+	// Although, we can use pointers as well
+
+	var f3 *Foo
+	f3 = &f
+	f3.A = 69
+	
+	fmt.Println(f3.A) // Since f3 points to f, the value assignemnt reflects on f as well
+	fmt.Println(f.A)
+
 }
