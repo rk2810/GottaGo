d240e56e7618a62e04e0a9d5d81200debc5898d9
rk2810 <rohan.kumquat@gmail.com>
2018-06-18 09:42:00 +0530

    Moar of structs. - embedded delegation in structs.
    Signed-off-by: rk2810 <rohan.kumquat@gmail.com>

---
+++ Structs/structs_advanced.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// Foo is a struct here
+type Foo struct {
+	A int
+	b string
+}
+
+// Bar is yet another struct that has F of Foo type
+type Bar struct {
+	C string
+	F Foo
+}
+
+// Baz has a struct declaration, this will show us embedded delegation
+type Baz struct {
+	D string
+	Foo
+}
+
+func main() {
+
+	f := Foo{A:10, b:"Hey"}
+	fmt.Println(f)
+
+	g := Bar{C:"Hola", F:f}
+	fmt.Println(g.F.A) // That's how you call values from that struct type var
+
+	h := Baz{D:"Hi", Foo:f}
+	fmt.Println(h.b) // That's how you call value from an embedded struct
+
+	var f1 Foo = h.Foo
+	fmt.Println(f1)
+}
