f5bde6f5bbfc1d4961660f08cd939be268922f66
rk2810 <rohan.kumquat@gmail.com>
2018-06-26 11:13:30 +0530

    Pass values in a Go routine.
    Signed-off-by: rk2810 <rohan.kumquat@gmail.com>

--- GoRoutines/routines.go
+++ GoRoutines/routines.go
@@ -5,17 +5,17 @@ import (
 	"fmt"
 )
 
-func runMe(){
-	fmt.Println("Hello from Go routine!")
+func runMe(name string){
+	fmt.Println("Hello to,", name, "from Go routine!")
 }
 func main(){
 	// We use WaitGroup instead of sleep
 	var wg sync.WaitGroup // We declare wg of type wait group
 	wg.Add(1) // add 1 go routine
-	go func() { // yep, A closure
-		runMe()
+	go func(name string) { // yep, A closure
+		runMe(name)
 		wg.Done()
-	}()
+	}("Rohan")
 
 	wg.Wait()
 }
