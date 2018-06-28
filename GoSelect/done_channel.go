433bf378e048096bacd42e93b3bae1360ffde528
rk2810 <rohan.kumquat@gmail.com>
2018-06-28 15:08:47 +0530

    Done channel basics. - Done channels are used to avoid swamping of resources.
    Signed-off-by: rk2810 <rohan.kumquat@gmail.com>

--- GoSelect/done_channel.go
+++ GoSelect/done_channel.go
@@ -1,31 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"time"
+	"fmt"
+)
 
 /*
 Go manages its resources but not for channels, If you do not close channels and clear buffer,
 You will end up swamping the resources. This can be done with done cahnnel.
 */
 
-func multiples(i int) chan int{	// takes integer argument and returns a channel
+func multiples(i int) (chan int, chan struct{}){	// takes integer argument and returns a channel
 	out := make(chan int)
+	done := make(chan struct{})
 	curVal := 0
 	go func(){ 
 		for{ // an infinite for loop that takes current value and doubles and increments it.
-			out <- curVal*2
-			curVal++
+			select{
+			case out <- curVal*i:
+				curVal++
+			case <- done:
+				fmt.Println("Closing go routine.")
+				return
+			}
 		}
 	}()
-	return out
+	return out, done
 }
 
 func main(){
-	twoCh := multiples(2) // twoCh calls func multiples and opens a channel
+	twoCh, done := multiples(2) // twoCh calls func multiples and opens a channel
 	for v := range twoCh{
 		if v > 20{ // value of v reaches 20 and loop breaks
 			break
 		}
 		fmt.Println(v)
 	}
-
+	close(done)
+	time.Sleep(2 * time.Second)
 }
